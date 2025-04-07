2f58c7f6aabdb6d41d01236587c1d91a47f8397b
V0yag3r <57376410+V0yag3r@users.noreply.github.com>
2025-04-08 01:23:02 +0200

    6
    6

--- script.js
+++ script.js
@@ -32,9 +32,13 @@ particlesJS('particles-js', {
 
 const content = {
   pl: `<h2>Kim jestem naprawdę?</h2>
-<p>Nazywam się <strong>Jacek Grodnicki</strong>. Mam ponad 18 lat praktyki w programowaniu...</p>`,
+<p>Nazywam się <strong>Jacek Grodnicki</strong>. Mam ponad 18 lat praktyki w programowaniu...</p>
+<p>Fizyka kwantowa to nie tylko matematyka i równania – to filozofia, sposób patrzenia na rzeczywistość, który wywraca nasze klasyczne postrzeganie świata do góry nogami. W tym blogu odkrywamy nie tylko fakty, ale też pytania, które warto zadawać. Czy rzeczywistość istnieje niezależnie od obserwatora? Jakie są granice poznania? Czy świadomość może mieć wpływ na fizykę? To tylko początek... <a href="#wiecej" style="color:#00f; text-decoration:underline;">więcej</a></p>
+`,
   en: `<h2>Who am I really?</h2>
-<p>My name is <strong>Jacek Grodnicki</strong>. I have over 18 years of experience...</p>`
+<p>My name is <strong>Jacek Grodnicki</strong>. I have over 18 years of experience...</p>
+<p>Fizyka kwantowa to nie tylko matematyka i równania – to filozofia, sposób patrzenia na rzeczywistość, który wywraca nasze klasyczne postrzeganie świata do góry nogami. W tym blogu odkrywamy nie tylko fakty, ale też pytania, które warto zadawać. Czy rzeczywistość istnieje niezależnie od obserwatora? Jakie są granice poznania? Czy świadomość może mieć wpływ na fizykę? To tylko początek... <a href="#wiecej" style="color:#00f; text-decoration:underline;">więcej</a></p>
+`
 };
 
 const introCard = document.getElementById("introCard");
